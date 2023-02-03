dccd6a4585caa6bddcd144828e15ee2866b52dab
wil.pannell <wil.pannell@gmail.com>
2023-02-03 13:31:42 -0800

    reformat/purge lint violations

--- src/App-New.js
+++ src/App-New.js
@@ -3,23 +3,24 @@ import React from 'react'
 import './App.css'
 import ZoomMtgEmbedded from '@zoomus/websdk/embedded'
 
-function App() {
+export default function App() {
   const client = ZoomMtgEmbedded.createClient()
 
   // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
-  var signatureEndpoint = ''
+  const signatureEndpoint = process.env.REACT_APP_SIGNATURE_ENDPOINT
   // This Sample App has been updated to use SDK App type credentials https://marketplace.zoom.us/docs/guides/build/sdk-app
-  var sdkKey = ''
-  var meetingNumber = '123456789'
-  var role = 0
-  var userName = 'React'
-  var userEmail = ''
-  var passWord = ''
+  const sdkKey = process.env.REACT_APP_ZOOM_SDK_KEY
+  const meetingNumber = process.env.REACT_APP_MEETING_NUMBER
+  const role = 0
+  const userName = 'React'
+  const userEmail = process.env.REACT_APP_USER_EMAIL
+  const password = process.env.REACT_APP_MEETING_PASSWORD
   // pass in the registrant's token if your meeting or webinar requires registration. More info here:
   // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/meetings#join-registered
   // Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/webinars#join-registered
-  var registrantToken = ''
+  const registrantToken = ''
 
+  let signature = ''
   function getSignature(e) {
     e.preventDefault()
 
@@ -32,8 +33,8 @@ function App() {
       }),
     })
       .then(res => res.json())
-      .then(response => {
-        startMeeting(response.signature)
+      .then(_ => {
+        startMeeting(signature)
       })
       .catch(error => {
         console.error(error)
@@ -43,45 +44,60 @@ function App() {
   function startMeeting(signature) {
     let meetingSDKElement = document.getElementById('meetingSDKElement')
 
-    client.init({
-      debug: true,
-      zoomAppRoot: meetingSDKElement,
-      language: 'en-US',
-      customize: {
-        meetingInfo: [
-          'topic',
-          'host',
-          'mn',
-          'pwd',
-          'telPwd',
-          'invite',
-          'participant',
-          'dc',
-          'enctype',
-        ],
-        toolbar: {
-          buttons: [
-            {
-              text: 'Custom Button',
-              className: 'CustomButton',
-              onClick: () => {
-                console.log('custom button')
-              },
-            },
+    client
+      .init({
+        debug: true,
+        zoomAppRoot: meetingSDKElement,
+        language: 'en-US',
+        customize: {
+          meetingInfo: [
+            'topic',
+            'host',
+            'mn',
+            'pwd',
+            'telPwd',
+            'invite',
+            'participant',
+            'dc',
+            'enctype',
           ],
+          toolbar: {
+            buttons: [
+              {
+                text: 'Custom Button',
+                className: 'CustomButton',
+                onClick: () => {
+                  console.log('custom button')
+                },
+              },
+            ],
+          },
         },
-      },
-    })
+      })
+      .then(() => {
+        console.log('init success')
+      })
+      .catch(error => {
+        console.log('init error', error)
+      })
 
-    client.join({
-      sdkKey: sdkKey,
-      signature: signature,
-      meetingNumber: meetingNumber,
-      password: passWord,
-      userName: userName,
-      userEmail: userEmail,
-      tk: registrantToken,
-    })
+    client
+      .join({
+        sdkKey: sdkKey,
+        signature: signature,
+        meetingNumber: meetingNumber,
+        password: password,
+        userName: userName,
+        userEmail: userEmail,
+        tk: registrantToken,
+      })
+      .then(res => {
+        console.log(JSON.stringify(res))
+        console.log('join meeting success')
+      })
+      .catch(error => {
+        console.log('join meeting error', error)
+      })
   }
 
   return (
@@ -94,10 +110,15 @@ function App() {
           {/* Zoom Meeting SDK Component View Rendered Here */}
         </div>
 
-        <button onClick={getSignature}>Join Meeting</button>
+        <button
+          onClick={e => {
+            if (signature) startMeeting(signature)
+            else getSignature(e)
+          }}
+        >
+          Join Meeting
+        </button>
       </main>
     </div>
   )
 }
-
-export default App
